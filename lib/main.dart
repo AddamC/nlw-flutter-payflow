b4177a293824260412c5609205856287ef0b0fa2
AddamC <addamcaue@hotmail.com>
2021-06-27 21:32:45 -0300

    aula 2 - finalização das telas splash, home e login

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SplashPage()
+    );
+  }
+}
